7f7b7ea66caf60f35c12379f8c1c4de1d8fe55ac
Ma Tanveer <065csebmatanveer@gmail.com>
2025-12-26 16:18:27 +0530

    addedplayerscript

--- My2DGAME/Assets/Scripts/Player.cs
+++ My2DGAME/Assets/Scripts/Player.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
-
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     // Update is called once per frame
     void Update()
     {
